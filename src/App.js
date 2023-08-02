ecf688285f32646d134393de6de7f8b9bfdf988a
Dmitriy <315overdose@gmail.com>
2023-08-02 21:51:54 +0300

    sort

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import MyButton from './components/UI/button/MyButton'
 import MyInput from './components/UI/input/MyInput'
 
 import PostForm from './components/PostForm'
+import MySelect from './components/UI/select/MySelect'
 
 function App() {
 	const [posts, setPosts] = useState([
-		{ id: 1, title: 'JavaScript 1', body: 'Description 1' },
-		{ id: 2, title: 'JavaScript 2', body: 'Description 2' },
-		{ id: 3, title: 'JavaScript 3', body: 'Description 3' },
-		{ id: 4, title: 'JavaScript 4', body: 'Description 4' },
+		{ id: 1, title: 'aaaa', body: 'ццц' },
+		{ id: 2, title: 'бббб', body: 'уууу' },
+		{ id: 3, title: 'вввв', body: 'аааа' },
+		{ id: 4, title: 'гггг', body: 'мммм' },
 	])
 
 	const addPostHandler = newPost => {
@@ -26,9 +27,28 @@ function App() {
 		setPosts(posts.filter(post => post.id !== postId))
 	}
 
+	const [selecteSort, setSelectedSort] = useState('')
+	const selecteSortHandler = sort => {
+		setSelectedSort(sort)
+		console.log('selected sort', sort)
+		setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+	}
+
 	return (
 		<div className="App">
 			<PostForm create={addPostHandler} />
+			<hr style={{ margin: '15px' }}></hr>
+			<div>
+				<MySelect
+					value={selecteSort}
+					onChange={selecteSortHandler}
+					options={[
+						{ value: 'title', name: 'По названию' },
+						{ value: 'body', name: 'По описанию' },
+					]}
+					defaultValue={'Сортировка по: '}
+				/>
+			</div>
 
 			{posts.length !== 0 ? (
 				<PostList
