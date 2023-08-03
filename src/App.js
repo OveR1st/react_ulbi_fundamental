057dd2e6e6b36a463378422d18154f420001595a
Dmitriy <315overdose@gmail.com>
2023-08-03 23:31:57 +0300

    react router v6.14

--- src/App.js
+++ src/App.js
@@ -1,94 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import PostService from './API/PostService'
+import React from 'react'
 
-import PostList from './components/PostList'
-import PostForm from './components/PostForm'
-import PostFilter from './components/PostFilter'
+import { createBrowserRouter, Link, Outlet, RouterProvider } from 'react-router-dom'
+import Posts from './pages/Posts'
+import About from './pages/About'
 
-import MyModal from './components/UI/MyModal/MyModal'
-import MyButton from './components/UI/button/MyButton'
-import Loader from './components/UI/Loader/Loader'
+export const Navbar = () => {
+	return (
+		<>
+			<div className="navbar_section">
+				<Link to="/">Home</Link>
+				<Link to="/posts">Posts</Link>
+				<Link to="/about">About</Link>
+			</div>
+			<Outlet />
+		</>
+	)
+}
 
-import './styles/App.css'
-
-import { usePosts } from './hooks/usePosts'
-import { useFetching } from './hooks/useFetching'
-import { usePagination } from './hooks/usePagination'
-
-import { getPageCount } from './utils/pages'
-import Pagination from './components/UI/pagination/Pagination'
+const router = createBrowserRouter([
+	{
+		element: <Navbar />,
+		children: [
+			{
+				path: '/',
+				element: <div>MAIN PAGE</div>,
+			},
+			{
+				path: '/posts',
+				element: <Posts />,
+			},
+			{
+				path: '/about',
+				element: <About />,
+			},
+		],
+	},
+])
 
 function App() {
-	const [posts, setPosts] = useState([])
-
-	const [filter, setFilter] = useState({ sort: '', query: '' })
-
-	const [modal, setModal] = useState(false)
-
-	const [totalPages, setTotalPages] = useState(0)
-
-	const [limit, setLimit] = useState(8)
-	const [page, setPage] = useState(1)
-
-	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-		const response = await PostService.getAll(limit, page)
-		const totalPages = getPageCount(response.headers['x-total-count'], limit)
-
-		setPosts(response.data)
-
-		setTotalPages(totalPages)
-	})
-
-	const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
-
-	const [pagesArray] = usePagination(totalPages)
-
-	const addPostHandler = newPost => {
-		setPosts([...posts, newPost])
-		setModal(false)
-	}
-
-	const deletePostHandler = postId => {
-		setPosts(posts.filter(post => post.id !== postId))
-	}
-
-	useEffect(() => {
-		fetchPosts()
-	}, [page])
-
 	return (
 		<div className="App">
-			<MyButton style={{ marginTop: '30px' }} onClick={fetchPosts}>
-				GET POSTS
-			</MyButton>
-
-			<MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
-				Создать пользователя
-			</MyButton>
-
-			<MyModal visible={modal} setModal={setModal}>
-				<PostForm create={addPostHandler} />
-			</MyModal>
-
-			<hr style={{ margin: '15px' }}></hr>
-
-			<PostFilter filter={filter} setFilter={setFilter} />
-
-			{postError && <h1>ERROR</h1>}
-
-			{isPostsLoading ? (
-				<div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
-					<Loader />
-				</div>
-			) : (
-				<PostList
-					deletePostHandler={deletePostHandler}
-					posts={sortedAndSearchPosts}
-					title={'Посты про JS'}
-				/>
-			)}
-
-			<Pagination pagesArray={pagesArray} page={page} setPage={setPage} />
+			<RouterProvider router={router} />
 		</div>
 	)
 }
